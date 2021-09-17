fb314e3441077b2bbcdef0fef8561abd247901c2
miconis <m.debonis991@gmail.com>
2021-09-17 10:33:29 +0200

    implementation of the graph visualization tool

--- dnet-dedup-test/src/test/java/eu/dnetlib/pace/DedupTestUtils.java
+++ dnet-dedup-test/src/test/java/eu/dnetlib/pace/DedupTestUtils.java
@@ -1,37 +1,57 @@
 package eu.dnetlib.pace;
 
+import eu.dnetlib.pace.config.Type;
+import eu.dnetlib.pace.model.FieldListImpl;
 import eu.dnetlib.pace.model.MapDocument;
 import eu.dnetlib.support.ConnectedComponent;
+import org.apache.commons.io.IOUtils;
 import org.apache.spark.api.java.JavaRDD;
+import org.codehaus.jackson.map.ObjectMapper;
+import scala.Tuple2;
 
+import java.io.*;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public abstract class DedupTestUtils {
 
+    public static String prepareTable(MapDocument doc) {
 
-//    public static void printStatistics(JavaRDD<ConnectedComponent> ccs){
-//        final JavaRDD<ConnectedComponent> connectedComponents = ccs.filter(cc -> cc.getDocs().size()>1);
-//        final JavaRDD<ConnectedComponent> nonDeduplicated = ccs.filter(cc -> cc.getDocs().size()==1);
-//
-//        //print deduped
-//        connectedComponents.map(cc -> {
-//            StringBuilder sb = new StringBuilder();
-//            for (MapDocument m : cc.getDocs()){
-//                sb.append(m.getFieldMap().get("originalId").stringValue() + " - "+ m.getFieldMap().get("legalname").stringValue() + "\n");
-//            }
-//            return sb.toString();
-//        }).foreach(s -> System.out.println("*******\n" + s + "*******\n"));
-//
-//        //print nondeduped
-//        nonDeduplicated.foreach(cc -> {
-//            System.out.println(cc.getId() + " - " + cc.getFieldMap().get("legalname").stringValue());
-//        });
-//
-//        System.out.println("Non duplicates: " + nonDeduplicated.count());
-//        System.out.println("Duplicates: " + connectedComponents.flatMap(cc -> cc.getDocs().iterator()).count());
-//        System.out.println("Connected Components: " + connectedComponents.count());
-//
-//    }
+        String ret = "<table>";
+
+        for(String fieldName: doc.getFieldMap().keySet()) {
+            if (doc.getFieldMap().get(fieldName).getType().equals(Type.String)) {
+                ret += "<tr><th>" + fieldName + "</th><td>" + doc.getFieldMap().get(fieldName).stringValue() + "</td></tr>";
+            }
+            else if (doc.getFieldMap().get(fieldName).getType().equals(Type.List)) {
+                ret += "<tr><th>" + fieldName + "</th><td>[" + ((FieldListImpl)doc.getFieldMap().get(fieldName)).stringList().stream().collect(Collectors.joining(";")) + "]</td></tr>";
+            }
+        }
+
+        return ret + "</table>";
+
+    }
+
+    public static void prepareGraphParams(List<String> vertexes, List<Tuple2<String, String>> edgesTuple, String filePath, String templateFilePath, Map<String, MapDocument> mapDocuments) {
+
+        List<Node> nodes = vertexes.stream().map(v -> new Node(v.substring(3, 20).replaceAll("_", ""), vertexes.indexOf(v), prepareTable(mapDocuments.get(v)))).collect(Collectors.toList());
+        List<Edge> edges = edgesTuple.stream().map(e -> new Edge(vertexes.indexOf(e._1()), vertexes.indexOf(e._2()))).collect(Collectors.toList());
+
+        try(FileWriter fw = new FileWriter(filePath)) {
+            String fullText = IOUtils.toString(new FileReader(templateFilePath));
+
+            String s = fullText
+                    .replaceAll("%nodes%", new ObjectMapper().writeValueAsString(nodes))
+                    .replaceAll("%edges%", new ObjectMapper().writeValueAsString(edges));
+
+            IOUtils.write(s, fw);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 
     public static String getOrganizationLegalname(MapDocument mapDocument){
         return mapDocument.getFieldMap().get("legalname").stringValue();
@@ -46,4 +66,66 @@ public abstract class DedupTestUtils {
         return "";
     }
 
+}
+
+class Node{
+    String label;
+    int id;
+    String title;
+
+    public Node(String label, int id, String title) {
+        this.label = label;
+        this.id = id;
+        this.title = title;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
+
+class Edge{
+    int from;
+    int to;
+
+    public Edge(int from, int to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    public int getFrom() {
+        return from;
+    }
+
+    public void setFrom(int from) {
+        this.from = from;
+    }
+
+    public int getTo() {
+        return to;
+    }
+
+    public void setTo(int to) {
+        this.to = to;
+    }
 }
